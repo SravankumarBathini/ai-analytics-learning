e201ad68be30bf267274197bef07211b468bd4ad
Sravan Kumar Bathini <shravanbathini2@gmail.com>
2026-07-20 17:04:40 +0530

    Add Python data types lesson

---
+++ 01-Python/02-Data-Types/data_types.py
@@ -0,0 +1,27 @@
+age = 32
+salary = 18.75
+company = "Accenture"
+is_manager = False
+
+print(type(age))          # Output: <class 'int'>
+print(type(salary))       # Output: <class 'float'>
+print(type(company))      # Output: <class 'str'>
+print(type(is_manager))   # Output: <class 'bool'>
+
+
+### Mini Exercise
+emp_id = 1001
+emp_name = "John Doe"
+salary = 75000.50
+bonus_eligible = True
+skills = ["Python", "Data Analysis", "Machine Learning"]
+location = ("New York", "USA")
+unique_departments = {"HR", "Finance", "IT", "Marketing"}
+
+print("Employee ID:", emp_id, type(emp_id))
+print("Employee Name:", emp_name, type(emp_name))
+print("Salary:", salary, type(salary))
+print("Bonus Eligible:", bonus_eligible, type(bonus_eligible))
+print("Skills:", skills, type(skills))
+print("Location:", location, type(location))
+print("Unique Departments:", unique_departments, type(unique_departments))
