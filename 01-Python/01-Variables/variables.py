6d516318e8c37a4144cdfa528249a6d869212a3d
Sravan Kumar Bathini <shravanbathini2@gmail.com>
2026-07-20 16:41:28 +0530

    Add Python variables lesson

---
+++ 01-Python/01-Variables/variables.py
@@ -0,0 +1,16 @@
+# ==========================================
+# Lesson 1: Variables in Python
+# Author: Shravan Bathini
+# Repository: AI Analytics Learning
+# ==========================================
+name = "Shravan Bathini"
+age = 32
+city = "Hyderabad"
+current_role = "Decision Science Practitioner Specialist"
+years_of_experience = 11
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Current Role:", current_role)
+print("Years of Experience:", years_of_experience)
