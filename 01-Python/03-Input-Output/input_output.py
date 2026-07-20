ddf0fa11a20a95d74ed4733e2f967ad259fb74e0
Sravan Kumar Bathini <shravanbathini2@gmail.com>
2026-07-20 22:47:22 +0530

    Add Python input and output lesson

---
+++ 01-Python/03-Input-Output/input_output.py
@@ -0,0 +1,20 @@
+# ==========================================
+# Lesson 3: Input & Output
+# ==========================================
+
+# print("Welcome to AI Analytics Learning!")
+
+
+# ##################################################
+# name = "Sravan Bathini"
+# experience = 11
+
+# print(f"Hello, {name}! You have {experience} years of experience.")
+
+#################################################
+
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to AI Analytics Learning.")
+
+age = int(input("Enter your age: "))
+print(f"Next year, you will be {age + 1} years old.")
